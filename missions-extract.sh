d75b8f4ad2b269ff727ab8458ce309955bfb485f
completely-burned <4268642+completely-burned@users.noreply.github.com>
2023-10-01 17:17:45 +0300

    Обновлён missions-extract.sh

--- missions-extract.sh
+++ missions-extract.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
-# Заметки.
-exit 1
+
+IFS=$'\n'
 
 # Путь к источнику.
-#IN=""
+IN=/var/www/armad/multi
 # Путь к назначению.
-#OUT=$(mktemp -t -d arma-extract.XXX)
+OUT=$(mktemp -t -d)
 
-cd ${OUT}
-# 7z.
-find ${IN} -type f -iname "*.7z"  -print -exec 7z -aos x {} *.pbo \;
-# RAR.
-find ${IN} -type f -iname "*.rar" -print -exec unrar x -o- {} \;
-# ZIP.
-find ${IN} -type f -iname "*.zip" -print -exec unzip -n {} *.pbo \;
+# Перемещение архивов к назначению.
+cd ${IN}
+for file in $(find . -type f -iname "*.7z" -or -iname "*.rar" -or -iname "*.zip")
+do
+	dir="${OUT}/$(dirname ${file})"
+	mkdir -p ${dir}
+	ln -s ${IN}/${file} ${dir}/
+done
+
+# Рукурсивная распаковка.
+count=1
+while [ $count -gt 0 ]
+do
+	count=0
+	cd ${OUT}
+
+	array=( $(find -L . -type f -iname "*.rar") )
+	if [ ${#array[@]} -gt 0 ]
+	then
+		count=$(($count+${#array[@]}))
+		for i in "${array[@]}"
+		do
+			dir="${OUT}/$(dirname ${i})"
+			if unrar x -o+ -ad ${i} ${dir}/
+			then
+				unlink ${i}
+			else
+				mv ${i} ${i}.skiped
+			fi
+		done
+	fi
+
+	array=( $(find -L . -type f -iname "*.7z") )
+	if [ ${#array[@]} -gt 0 ]
+	then
+		count=$(($count+${#array[@]}))
+		for i in "${array[@]}"
+		do
+			dir="${OUT}/$(dirname ${i})"
+			if 7z x -aoa -o${dir}/* -- ${i}
+			then
+				unlink ${i}
+			else
+				mv ${i} ${i}.skiped
+			fi
+		done
+	fi
+
+	array=( $(find -L . -type f -iname "*.zip") )
+	if [ ${#array[@]} -gt 0 ]
+	then
+		count=$(($count+${#array[@]}))
+		for i in "${array[@]}"
+		do
+			dir="${i}.x"
+			if unzip -o ${i} -d ${dir}/
+			then
+				unlink ${i}
+			else
+				mv ${i} ${i}.skiped
+			fi
+		done
+	fi
+done
